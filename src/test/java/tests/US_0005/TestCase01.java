6ce3c8d9a0cd3f877a2d4cee5e408d63b98b97bd
Hasan F <hfidan006@gmail.com>
2022-02-11 17:58:14 +0100

    testCase01yazildi

--- src/test/java/tests/US_0005/TestCase01.java
+++ src/test/java/tests/US_0005/TestCase01.java
@@ -1,4 +1,11 @@
 package tests.US_0005;
 
 public class TestCase01 {
+
+
+
+
+
+
+    //999999999999999
 }
--- src/test/java/tests/US_0009/TestCase01.java
+++ src/test/java/tests/US_0009/TestCase01.java
@@ -2,6 +2,12 @@ package tests.US_0009;
 
 public class TestCase01 {
 
+
+
     //Helloo
     //Hi
+
+
+
+
 }
