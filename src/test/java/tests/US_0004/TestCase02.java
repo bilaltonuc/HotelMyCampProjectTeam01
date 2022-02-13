ccd861b24d0719efd6602bc703e9aeb6d1549316
Hasan F <hfidan006@gmail.com>
2022-02-13 21:40:46 +0100

    14.02_utilitiese ekleme yapildi

--- src/test/java/tests/US_0004/TestCase02.java
+++ src/test/java/tests/US_0004/TestCase02.java
@@ -1,9 +1,14 @@
 package tests.US_0004;
 
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HotelMyCampHotelList;
 import pages.HotelMyCampMain;
 import utilities.Driver;
+import utilities.ReusableMethods;
+
+import java.io.IOException;
 
 public class TestCase02 {
 
@@ -11,13 +16,27 @@ public class TestCase02 {
     //Adresindeki 'ADD HOTEL' butonu gorunuyor ve tiklanabiliyor olmali."
 
     @Test
-    public void test02() {
+    public void test02() throws IOException {
 
         HotelMyCampMain hotelMyCamp = new HotelMyCampMain();
+        HotelMyCampHotelList hotelList = new HotelMyCampHotelList();
+
+        hotelMyCamp.loginOl();
+
         hotelMyCamp.hotelManagementButtonElementi.click();
         hotelMyCamp.hotelListButtonElementi.click();
-        hotelMyCamp.addHotelButtonElementi.click();
 
+        WebElement addHotelButtonElementi = hotelMyCamp.addHotelButtonElementi;
+        Assert.assertTrue(addHotelButtonElementi.isDisplayed());
+
+        addHotelButtonElementi.click();
+
+        ReusableMethods.getScreenshot("AddHotelPage");
+
+        String expectedUrl = "https://www.hotelmycamp.com/admin/HotelAdmin/Create";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+
+        Assert.assertEquals(actualUrl, expectedUrl);
 
 
 
