11adfe09e175b70860d661f4f7abf17d5395af9d
Hasan F <hfidan006@gmail.com>
2022-02-13 21:02:26 +0100

    14.002

--- src/test/java/tests/US_0004/TestCase01.java
+++ src/test/java/tests/US_0004/TestCase01.java
@@ -18,35 +18,21 @@ public class TestCase01 {
 // 'https://www.hotelmycamp.com/admin/HotelAdmin' adresine yonlendiriyor olmali.
 
 
-    @Test(invocationCount = 500)
-    public void test01() {
+    @Test
+    public void test01() throws IOException {
+
         HotelMyCampHotelList hotelList = new HotelMyCampHotelList();
         HotelMyCampMain hotelMyCamp = new HotelMyCampMain();
         hotelMyCamp.loginOl();
-        hotelMyCamp.hotelManagementButtonElementi.click();
-        hotelMyCamp.hotelListButtonElementi.click();
-        hotelMyCamp.addHotelButtonElementi.click();
-        hotelList.addHotelCodeBoxElementi.sendKeys("13579");
-        hotelList.addHotelNameBoxElementi.sendKeys("Deneme Hotel");
-        hotelList.addHotelAddressBoxElementi.sendKeys("9 E. Loockerman street, suite 311 Dover, DE 19901");
-        hotelList.addHotelPhoneBoxElementi.sendKeys("302 718 2180");
-        hotelList.addHotelEmailBoxElementi.sendKeys("info@hotelmycamp.com");
-        WebElement dropDownElementi = hotelList.addHotelIDGroupDropDownElementi;
-        Select select = new Select(dropDownElementi);
-        select.selectByIndex(1);
-        hotelList.addHotelSaveButtonElementi.click();
 
-        String expectedResultText = "Hotel was inserted successfully";
+        String expectedUrl ="https://www.hotelmycamp.com/Admin/UserAdmin";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+        Assert.assertEquals(actualUrl,expectedUrl);
 
-        String actualResultText = hotelList.addHotelAlertMesajiElementi.getText();
-        Assert.assertTrue(actualResultText.contains(expectedResultText));
-
-        hotelMyCamp.bekle(1);
-        WebElement alertOkButtonElementi = hotelList.addHotelAlertOkButtonElementi;
-        alertOkButtonElementi.click();
-
+        ReusableMethods.getScreenshot("LoginBasarili");
 
         Driver.closeDriver();
+
     }
 
 
--- src/test/java/tests/US_0004/TestCase02.java
+++ src/test/java/tests/US_0004/TestCase02.java
@@ -1,6 +1,100 @@
 package tests.US_0004;
 
+import org.testng.annotations.Test;
+import pages.HotelMyCampHotelList;
+import pages.HotelMyCampMain;
+import utilities.Driver;
+
 public class TestCase02 {
+
+
+    @Test
+    public void test02() {
+
+        HotelMyCampMain hotelMyCamp = new HotelMyCampMain();
+        hotelMyCamp.hotelManagementButtonElementi.click();
+        hotelMyCamp.hotelListButtonElementi.click();
+        hotelMyCamp.addHotelButtonElementi.click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+        String code ="13579";
+        hotelList.addHotelCodeBoxElementi.sendKeys(code);
+
+        String hotelName = "Deneme Hotel";
+        hotelList.addHotelNameBoxElementi.sendKeys(hotelName);
+
+        String hotelAddress ="9 E. Loockerman street, suite 311 Dover, DE 19901";
+        hotelList.addHotelAddressBoxElementi.sendKeys(hotelAddress);
+
+        String hotelPhone="302 718 2180";
+        hotelList.addHotelPhoneBoxElementi.sendKeys(hotelPhone);
+
+        String hotelMail = "info@hotelmycamp.com";
+        hotelList.addHotelEmailBoxElementi.sendKeys(hotelMail);
+
+
+        WebElement dropDownElementi = hotelList.addHotelIDGroupDropDownElementi;
+        Select select = new Select(dropDownElementi);
+        select.selectByIndex(1);
+        hotelList.addHotelSaveButtonElementi.click();
+
+        hotelMyCamp.bekle(1);
+        ReusableMethods.getScreenshot("Save");
+
+        String expectedResultText = "Hotel was inserted successfully";
+        String actualResultText = hotelList.addHotelAlertMesajiElementi.getText();
+        Assert.assertTrue(actualResultText.contains(expectedResultText));
+
+        WebElement alertOkButtonElementi = hotelList.addHotelAlertOkButtonElementi;
+        alertOkButtonElementi.click();
+*/
+
+        Driver.closeDriver();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
-  //testcase05
 }
