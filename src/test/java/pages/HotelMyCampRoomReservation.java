7bf10603fe5af4b5b1cdbfe0210909c37a5b6aaa
Hasan Fidan <94446886+hfidan06@users.noreply.github.com>
2022-02-15 19:17:55 +0100

    1502 (#32)
    * 10.02
    
    * first
    
    * first
    
    * second
    
    * first
    
    * second
    
    * second
    
    * last
    
    * last
    
    * last
    
    * last
    
    * first
    
    * second
    
    * second
    
    * last
    
    * last
    
    * last
    
    * last
    
    * US_0009
    
    Co-authored-by: Zeynep Arslan <a.zeynep@gmail.com>

--- src/test/java/pages/HotelMyCampRoomReservation.java
+++ src/test/java/pages/HotelMyCampRoomReservation.java
@@ -1,10 +1,13 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
-public class HotelMyCampRoomReservation {
+import java.util.List;
 
+public class HotelMyCampRoomReservation {
 
     public HotelMyCampRoomReservation() {
 
@@ -12,31 +15,35 @@ public class HotelMyCampRoomReservation {
 
     }
 
-
     //               USER STORY 8  9  VE  10         //
     //               Room Reservation               //
 
 
+    // US_0009
+    @FindBy(xpath = "//option[@value='-1']")
+    public WebElement viewAllButtonFirst;
 
+    @FindBy(xpath = "//tbody//tr")
+    public List<WebElement> rawsList1;
 
+    @FindBy(xpath = "//tbody//td")
+    public List<WebElement> dataList1;
 
+    @FindBy(xpath = "//div[contains(@class,'col-md-8 col-sm-12')]//button[contains(@class,'btn green')][normalize-space()='Save']")
+    public WebElement saveButtonGeneralData;
 
+    @FindBy(xpath = "//button[@onclick='javascript:AddBarcode();return false;']")
+    public WebElement saveButtonProperty;
 
+    @FindBy(xpath = "//option[@value='-1']")
+    public WebElement viewAllButtonSecond;
 
+    @FindBy(xpath = "//tbody//tr")
+    public List<WebElement> rawsList2;
 
+    @FindBy(xpath = "//tbody//td")
+    public List<WebElement> dataList2;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @FindBy(id = "btnDelete")  // //button[@id='btnDelete']
+    public WebElement deleteButton;
 }
